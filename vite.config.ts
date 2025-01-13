8731fda64954f64c66b1e2bac06f65eb83182b45
Yeongjae-Kong <kong7300@dgist.ac.kr>
2025-01-13 18:13:50 +0900

    modify viteconfigts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['@pmndrs/branding'], // 외부 모듈로 처리
+    },
+  },
   server: {
-    port: 3000,
+    host: "::",
+    port: 8080,
   },
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
