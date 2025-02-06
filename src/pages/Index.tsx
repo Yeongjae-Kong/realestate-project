89400a436cfbd7be8a7f5412117a00be7221f7df
Yeongjae-Kong <kong7300@dgist.ac.kr>
2025-02-07 02:14:38 +0900

    add mobile floating nav button

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -228,7 +228,7 @@ const Index = () => {
     };
   }, []);
 
-  // section 글 애니메이션
+  // section1 글 애니메이션
   useEffect(() => {
     const timeouts = fadeInElements.map((_, index) => {
       return setTimeout(() => {
@@ -284,7 +284,7 @@ const Index = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsFadeIn(true);
-    }, 2000); // 2초 후에 fade-in 시작
+    }, 1000); // 2초 후에 fade-in 시작
 
     return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 정리
   }, []);
